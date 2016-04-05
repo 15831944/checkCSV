668a6b0bec30fcf08aac5d09740f8c7edb6abf3e
hostage112 <ess.aleksandr@gmail.com>
2016-04-05 23:44:50 +0300

    added file

---
+++ checkCSV/exportModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkCSV
+{
+    class exportModule
+    {
+        string _path;
+        List<ElementData> _parts;
+
+        public exportModule(string path, List<ElementData> parts)
+        {
+            _path = path;
+            _parts = parts;
+        }
+    }
+}
